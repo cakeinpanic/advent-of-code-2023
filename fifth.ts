661dc80d7cc8a414f42fc29080e422d67f925f05
Katya Pavlenko <kpavlenko@yotpo.com>
2023-12-06 00:28:50 +0200

    solve 5+

--- fifth.ts
+++ fifth.ts
@@ -77,8 +77,19 @@ function parseData(str: string): Data {
   };
 }
 
-function buildMap(mappings: Mapping[]): Mapping[] {
+function getSeedRange(pair: number[]): number[] {
 
+  const [seed1, range] = pair;
+  const newSeeds = [];
+
+  for (let i = 0; i <= range; i++) {
+    newSeeds.push(seed1 + i);
+  }
+
+  return newSeeds;
+}
+
+function buildMap(mappings: Mapping[]): Mapping[] {
   mappings = mappings.sort((m1, m2) => {
     return m1.source > m2.source ? 1 : -1;
   });
@@ -92,32 +103,79 @@ function getCoordinate(from: number, mapping: Mapping[]): number {
     if (isHit) {
       return;
     }
-    if (from >= source && from <= source + range) {
+    if (from >= source && from < source + range) {
       isHit = true;
       from = destination + from - source;
     }
   });
 
   return from;
-
 }
 
-function mapAll(str: string): any {
-  const data = parseData(str);
-
-  const maps = data.mappings.map(({ mapping }) => buildMap(mapping));
-
-  const final = data.seeds.map((seed) => {
-    let coordinate = seed;
+function processSeedsRange(seeds: number[], maps: Mapping[][]): number {
+  let min = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < seeds.length; i++) {
+    let coordinate = seeds[i];
     for (let j = 0; j < maps.length; j++) {
       const c = getCoordinate(coordinate, maps[j]);
       coordinate = c === undefined ? coordinate : c;
     }
-    return coordinate;
-  });
+    if (min > coordinate) {
+      min = coordinate;
+    }
+  }
 
-  const min = Math.min(...final);
-  console.log(min);
+  return min;
 }
 
-mapAll(input);
+function splitPair(pair: number[]): number[][] {
+  const [from, range] = pair;
+  const middle = Math.floor(range / 2);
+  return [
+    [from, middle],
+    [from + Math.ceil(range / 2), middle]
+  ];
+}
+function mapAllByPairs(data: Data): any {
+  const maps = data.mappings.map(({ mapping }) => buildMap(mapping));
+  //const mins = [522761994,3660193833,1994231379,668015448,288875640,232257988,280278922,555394663,2124094240,2130440815,1813446793,1833647722];
+  //7 [308079844,327230151,2459750215,1194105161]
+  //8 [1538233289,2185751788,1399376623,1480285602,81956384,1028979955,2076596363,921962153]
+  // 9 [1116311642,606585628,1944266713,2282273940,2403419102,416908948,484511758,610263099]
+
+  const mins = [];
+
+  const seedPairs = data.seeds.map((seed, i) => i % 2 ? null : [seed, data.seeds[i + 1]])
+    .filter(Boolean)
+    .slice(10,11)
+    .map(splitPair).flat()
+    .map(splitPair).flat()
+    .map(splitPair).flat()
+
+  for (let i = 0; i < seedPairs.length; i++) {
+    let seedsRange = getSeedRange(seedPairs[i]);
+    console.log('range formed');
+    const min = processSeedsRange(seedsRange, maps);
+    seedsRange = null;
+    mins.push(min);
+    console.log('pair finished');
+    console.log(JSON.stringify(mins));
+  }
+
+  const superMin = Math.min(...mins);
+  console.log(superMin);
+}
+
+function mapAll(data: Data): any {
+  const maps = data.mappings.map(({ mapping }) => buildMap(mapping));
+  const min = processSeedsRange(data.seeds, maps);
+  console.log(min);
+
+}
+
+// first
+//mapAll(parseData(input1));
+
+//second
+mapAllByPairs((parseData(input)));
+
