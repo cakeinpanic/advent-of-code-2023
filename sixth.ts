625a73939fcc13da6b158de6ff661410bdd512e9
Katya Pavlenko <kpavlenko@yotpo.com>
2023-12-06 23:18:25 +0200

    solve 6

--- sixth.ts
+++ sixth.ts
@@ -5,12 +5,31 @@ const input1 = `Time:      7  15   30
 Distance:  9  40  200`;
 
 function parseInput(str: string) {
-const d =str.replace(/\w+:/g, '').replace(/\s+/g, ' ').split(' ').map((t) => +t)
-  console.log(d);
+  return str.replace(/\w+:/g, '').replace(/ +/g, ' ').split('\n').map((x) => x.trim().split(' ').map((y) => parseInt(y, 10)));
+
+}
+
+function processRace(time: number, distance: number): number[] {
+  let result = [];
+  let speed = 0;
+  for (let i = 0; i <= time; i++) {
+    speed = i
+    const res = speed * (time - i);
+    if (res > distance) {
+      result.push(i);
+    }
+  }
+  //console.log(time, result);
+  return result;
 }
 
 function solve(inp: string) {
-const data = parseInput(inp);
+  const data = parseInput(inp);
+  const res = data[0].map((time, i) => {
+    const variants = processRace(time, data[1][i])
+    return variants.length
+  }).reduce((a,b)=>a*b, 1)
+  console.log(res);
 }
 
-solve(input1)
+solve(input);
